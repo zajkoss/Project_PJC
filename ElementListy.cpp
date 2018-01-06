6c6ef417691c10d0d6b6329a8e0bb3b47da551b7
Lukasz <zajkowskilukas@gmail.com>
2018-01-06 15:19:35 +0100

    add ElementyList.cpp , getters/setters, construktors

---
+++ ElementListy.cpp
@@ -0,0 +1,30 @@
+#include "stdafx.h"
+#include "ElementListy.h"
+#include <iostream>
+
+ElementListy::ElementListy(Liczba  *value) : liczba(value){
+	cout << "Konstruktor element listy" << endl;
+}
+
+ElementListy* ElementListy::wezNastepnyElement() {
+		return this->nastepnyElement;
+}
+
+ElementListy* ElementListy::wezPoprzedniElement() {
+	return this->poprzedniElement;
+}
+
+void ElementListy::ustawNastepnyElement(ElementListy * value) {
+
+		this->nastepnyElement = value;
+
+}
+
+void ElementListy::ustawPoprzedniElement(ElementListy * value) {
+	this->poprzedniElement = value;
+}
+
+char* ElementListy::wezLiczbe() {
+	return  this->liczba->Wartosc();
+
+}
