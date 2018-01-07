d55f8f8486eee835eb38974afbaada0192654566
Lukasz <zajkowskilukas@gmail.com>
2018-01-07 19:44:13 +0100

    change unsigned intto int , add operator << and add porownajLiczby method

--- ListaDuzychLiczb.h
+++ ListaDuzychLiczb.h
@@ -1,23 +1,30 @@
 #pragma once
+#include "DuzaLiczba.h"
 #include "ElementListy.h"
+#include <iostream>
+using namespace std;
 
 class ListaDuzychLiczb {
 	ElementListy *pierwszy;
 	ElementListy *ostatni;
-	unsigned int rozmiar;
+	int rozmiar;
+
 public:
 	ListaDuzychLiczb();
 
 	//meotdy wstaw
 	void wstawNaPoczatek(Liczba *);
 	void wstawNaKoniec(Liczba *);
-	void wstawNaPozycje(Liczba *,unsigned int);
+	void wstawNaPozycje(Liczba *,int);
 
 	//metody usun
 	void usunPierwszyElement();
 	void usunOstatniElement();
-	void usunPozycje(unsigned int);
+	void usunPozycje( int);
 	void usunWszystkieElementy();
 
 	void sortuj();
+	bool porownajLiczby(char*, char*);
+
+	friend ostream& operator<<(ostream&, const ListaDuzychLiczb&);
 };
